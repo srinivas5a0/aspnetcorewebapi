8f68cca65be95dda08452b8293556e8833bc894c
srinivasulu poluboyena <srinivas5a0@gmail.com>
2018-05-09 20:22:21 +0530

    deleted

--- Models/PointOfSale.cs
+++
@@ -1,9 +0,0 @@
-namespace RetailPlatformApp.Models{
-
-    public class PointOfSale {
-        public long Id { get; set; }
-        public string POSName { get; set; }
-        public int POSNumber { get; set; }
-        public POSType POSType { get; set; }
-    }
-}
